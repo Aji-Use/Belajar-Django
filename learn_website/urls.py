4e635ff7dd602437a3ca18af507c69a343a96de6
Aji-Use <baguskuncoroa123@gmail.com>
2025-06-29 04:53:52 -0700

    update landing page

--- learn_website/urls.py
+++ learn_website/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index, name='home'),
     path('library/', include('library.urls')),
     path('manager/', include('manager.urls')),
     path('blog/', include('blog.urls'))
